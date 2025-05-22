009885782e2fa7efea4927d934ed248832949662
dev.davidduque@gmail.com <opcodemaster>
2025-05-21 21:40:16 -0500

    Proyecto inicializado con spring y maven para manejo de dependencias

---
+++ FirstActivity/src/main/java/com/SpringCourse/FirstActivity/FirstActivityApplication.java
@@ -0,0 +1,13 @@
+package com.SpringCourse.FirstActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstActivityApplication.class, args);
+	}
+
+}
---
+++ FirstActivity/src/test/java/com/SpringCourse/FirstActivity/FirstActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCourse.FirstActivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
